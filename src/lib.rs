ab034b5aa598cd0a916d77958b65769253dcfb32
Dennis Kievits <denkievits@gmail.com>
2022-11-16 22:05:46 +0100

    poc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use diesel::backend::Backend;
+use diesel::expression::is_aggregate;
+use diesel::expression::MixedAggregates;
+use diesel::expression::ValidGrouping;
+use diesel::sql_types::Bool;
+use diesel::sql_types::Text;
+use diesel::AppearsOnTable;
+use diesel::BoxableExpression;
+use diesel::Expression;
+use diesel::TextExpressionMethods;
+
+mod schema;
+
+enum StringFilter {
+    Equals(String),
+    StartsWith(String),
+}
+
+fn string_filter<T, C, DB>(
+    column: C,
+    filter: StringFilter,
+) -> Box<dyn BoxableExpression<T, DB, SqlType = Bool>>
+where
+    DB: Backend,
+    C: TextExpressionMethods + Expression<SqlType = Text> + AppearsOnTable<T> + ValidGrouping<()>,
+    is_aggregate::No: MixedAggregates<C::IsAggregate, Output = is_aggregate::No>,
+{
+    match filter {
+        StringFilter::Equals(other) => Box::new(column.eq(other)),
+        StringFilter::StartsWith(other) => Box::new(column.like(format!("{}%", other))),
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +37,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut statement = crate::schema::users::table.into_boxed();
+        let filter = StringFilter::Equals("John Doe".to_string());
+        statement = statement.filter(string_filter(crate::schema::users::name, filter));
     }
 }
